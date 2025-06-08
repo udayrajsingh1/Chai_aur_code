d78a1a17a5683c826013efa322f9a0021c94ff2a
udayrajsingh1 <Udayrajsingh1319@gmail.com>
2025-06-08 22:47:02 +0000

    objects and array in function

--- 01_basics/02_functions.js
+++ 01_basics/02_functions.js
@@ -5,4 +5,37 @@ function loginUserMessage(username){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Uday Raj Singh"))
+// console.log(loginUserMessage("Uday Raj Singh"))
+
+// ---> Normal function return
+function calculatCartPrice(val1){
+    return val1
+}
+// console.log(calculatCartPrice(20))
+
+
+// ---> Now using ... as rest operator when there is multiple values to pass as arguments 
+function calculatCartPrice(...val1){
+    return val1
+}
+// console.log(calculatCartPrice(20,20,23,54))
+
+
+// ---> handling object in fucntions
+const user = {
+    name: "Uday",
+    age: 20
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and age is ${anyobject.age}`)
+}
+// handleObject(user)
+
+
+// ---> Handling array inside a function
+const myArray = [100, 200, 300, 400]
+function secondValue(getarray){
+    return myArray[1]
+}
+// console.log(secondValue(myArray))
